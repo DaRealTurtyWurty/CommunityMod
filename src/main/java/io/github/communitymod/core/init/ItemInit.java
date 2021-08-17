69ca9dd73ff090e08e2bfe6a86ead5301cf1a815
Jaime Abril <jablazr@gmail.com>
2021-08-18 00:30:20 +0200

    Fixed formatting

--- src/main/java/io/github/communitymod/core/init/ItemInit.java
+++ src/main/java/io/github/communitymod/core/init/ItemInit.java
@@ -1,13 +1,10 @@
 package io.github.communitymod.core.init;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import io.github.communitymod.CommunityMod;
+import io.github.communitymod.common.armor.BeanArmorMaterial;
 import io.github.communitymod.common.items.MiguelItem;
 import io.github.communitymod.common.items.OrbOfInsanity;
 import io.github.communitymod.common.items.SpecialItem;
-import io.github.communitymod.common.armor.BeanArmorMaterial;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -27,42 +24,48 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @EventBusSubscriber(modid = CommunityMod.MODID, bus = Bus.MOD)
 public final class ItemInit {
 
-  static final Set<RegistryObject<Block>> BLOCK_ITEM_BLACKLIST = new HashSet<>();
+	static final Set<RegistryObject<Block>> BLOCK_ITEM_BLACKLIST = new HashSet<>();
 
-  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CommunityMod.MODID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+			CommunityMod.MODID);
 
-  public static final RegistryObject<Item> BEANS = ITEMS.register("beans",
-      () -> new Item(new Item.Properties().tab(CommunityMod.TAB).stacksTo(69).fireResistant()));
+	public static final RegistryObject<Item> BEANS = ITEMS.register("beans",
+			() -> new Item(new Item.Properties().tab(CommunityMod.TAB).stacksTo(69).fireResistant()));
 
-  public static final RegistryObject<Item> LONEWOLF_USER = ITEMS.register("lonewolf_user",
-      () -> new Item(new Item.Properties().tab(CommunityMod.TAB)));
+	public static final RegistryObject<Item> LONEWOLF_USER = ITEMS.register("lonewolf_user",
+			() -> new Item(new Item.Properties().tab(CommunityMod.TAB)));
+  
+	public static final FoodProperties BEAN_SOUP_PROPERTIES = (new FoodProperties.Builder().nutrition(8).saturationMod(0.6F)
+		.effect(() -> new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 7200, 0), 1.0F)
+		.effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.5F)
+		.alwaysEat().build()
+	);
 
-  public static final FoodProperties BEAN_SOUP_PROPERTIES = (new FoodProperties.Builder().nutrition(8)
-      .saturationMod(0.6F).effect(() -> new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 7200, 0), 1.0F)
-      .effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.5F).alwaysEat().build());
-
-  public static final RegistryObject<Item> BEAN_SOUP = ITEMS.register("bean_soup", () -> new BowlFoodItem(
-      new Item.Properties().tab(CommunityMod.TAB).food(BEAN_SOUP_PROPERTIES).stacksTo(1).fireResistant()));
-
+	public static final RegistryObject<Item> BEAN_SOUP = ITEMS.register("bean_soup",
+			() -> new BowlFoodItem(new Item.Properties().tab(CommunityMod.TAB).food(BEAN_SOUP_PROPERTIES).stacksTo(1).fireResistant()));
+  
   public static final RegistryObject<Item> BEANS_SANDWICH = ITEMS.register("beans_sandwich",
-      () -> new Item(new Item.Properties().tab(CommunityMod.TAB)
-          .food(new FoodProperties.Builder().saturationMod(35.6F).meat().nutrition(20).build())));
+			() -> new Item(new Item.Properties().tab(CommunityMod.TAB).food(new FoodProperties.Builder().saturationMod(35.6F).meat().nutrition(20).build())));
 
-  public static final RegistryObject<Item> MIGUEL_OF_FORTUNE = ITEMS.register("miguel_of_fortune",
-      () -> new MiguelItem(new Item.Properties().tab(CommunityMod.TAB)));
-
-  public static final RegistryObject<Item> BEAN_HAT = ITEMS.register("bean_hat",
-      () -> new ArmorItem(BeanArmorMaterial.BEAN_ARMOR, EquipmentSlot.HEAD,
-          new Item.Properties().tab(CommunityMod.TAB)));
+	
+	public static final RegistryObject<Item> MIGUEL_OF_FORTUNE = ITEMS.register("miguel_of_fortune", 
+			() -> new MiguelItem(new Item.Properties().tab(CommunityMod.TAB)));
 
 	public static final RegistryObject<Item> SPECIAL_ITEM = ITEMS.register("special_item",
 			() -> new SpecialItem(new Item.Properties().tab(CommunityMod.TAB)));
 
 	public static final RegistryObject<Item> ORB_OF_INSANITY = ITEMS.register("orb_of_insanity",
 			() -> new OrbOfInsanity(new Item.Properties().tab(CommunityMod.TAB).stacksTo(1).rarity(Rarity.UNCOMMON)));
+
+    public static final RegistryObject<Item> BEAN_HAT = ITEMS.register("bean_hat",
+            () -> new ArmorItem(BeanArmorMaterial.BEAN_ARMOR, EquipmentSlot.HEAD,
+                new Item.Properties().tab(CommunityMod.TAB)));
 
 	@SubscribeEvent
 	public static void registerBlockItems(final RegistryEvent.Register<Item> event) {
