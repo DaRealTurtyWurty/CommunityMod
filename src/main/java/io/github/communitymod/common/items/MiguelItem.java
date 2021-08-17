3b77649c3a023216c9c52dd5a7947d93472e29a4
jEnS <jens1433@gmail.com>
2021-08-17 21:39:32 +0200

    Revert "Formatting and different use override"
    This reverts commit 2ed416edd649ccdde75f0c440a8f7dd7057f5b8d.

--- src/main/java/io/github/communitymod/common/items/MiguelItem.java
+++ src/main/java/io/github/communitymod/common/items/MiguelItem.java
@@ -2,14 +2,12 @@ package io.github.communitymod.common.items;
 
 import java.util.Random;
 
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemCooldowns;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.context.UseOnContext;
 
 public class MiguelItem extends Item {
 
@@ -18,31 +16,33 @@ public class MiguelItem extends Item {
 	}
 	
 	@Override
-	public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
-		ItemCooldowns itemCoolDowns = pPlayer.getCooldowns();
+	public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
 		
-		if(!pLevel.isClientSide())
+		ItemCooldowns itemCoolDowns = context.getPlayer().getCooldowns();
+		
+		if(!context.getLevel().isClientSide())
 		{
-			if(!itemCoolDowns.isOnCooldown(pPlayer.getItemInHand(pHand).getItem()))
+			if(!itemCoolDowns.isOnCooldown(stack.getItem()))
 			{
 				Random rn = new Random();
 				int i = rn.nextInt(100);
 				if(i == 1)
 				{
-					pPlayer.spawnAtLocation(Items.DIAMOND);
+					context.getPlayer().spawnAtLocation(Items.DIAMOND);
 				}
 				else if(i > 90)
 				{
-					pPlayer.spawnAtLocation(Items.IRON_INGOT);
+					context.getPlayer().spawnAtLocation(Items.IRON_INGOT);
 				}
 				else {
-					pPlayer.spawnAtLocation(Items.DIRT);
+					context.getPlayer().spawnAtLocation(Items.DIRT);
 				}
-				itemCoolDowns.addCooldown(pPlayer.getItemInHand(pHand).getItem(), 200);
+				itemCoolDowns.addCooldown(stack.getItem(), 200);
 			}
+			
 		}
 		
-		return super.use(pLevel, pPlayer, pHand);
+		return InteractionResult.SUCCESS;
 	}
 
 }
