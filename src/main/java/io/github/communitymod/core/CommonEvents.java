5fbf83ac40fd235424ef028088fd3d643cc9d412
PeterGamesGR <46004440+PeterGamesGR@users.noreply.github.com>
2021-08-18 22:35:06 +0300

    Fixed CommonEvents being messed up.

--- src/main/java/io/github/communitymod/core/CommonEvents.java
+++ src/main/java/io/github/communitymod/core/CommonEvents.java
@@ -1,6 +1,5 @@
 package io.github.communitymod.core;
 
-import com.mojang.serialization.Codec;
 import io.github.communitymod.CommunityMod;
 import io.github.communitymod.common.entities.BeanEntity;
 import io.github.communitymod.core.init.DimensionInit;
@@ -11,9 +10,7 @@ import io.github.communitymod.core.world.structures.ConfiguredStructures;
 import io.github.communitymod.core.world.structures.bean.BeanPieces;
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.StructureSettings;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraft.world.level.levelgen.feature.StructurePieceType;
@@ -26,16 +23,17 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
-import org.apache.logging.log4j.LogManager;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
 public final class CommonEvents {
+
+    private static StructurePieceType register(StructurePieceType structurePiece, String key) {
+        return Registry.register(Registry.STRUCTURE_PIECE, key.toLowerCase(Locale.ROOT), structurePiece);
+    }
 
     @EventBusSubscriber(modid = CommunityMod.MODID, bus = Bus.FORGE)
     public static final class ForgeEvents {
@@ -55,46 +53,45 @@ public final class CommonEvents {
             }
         }
 
-        @EventBusSubscriber(modid = CommunityMod.MODID, bus = Bus.MOD)
-        public static final class ModEvents {
+    }
 
-            @SubscribeEvent
-            public static void commonSetup(final FMLCommonSetupEvent event) {
-                DimensionInit.setup();
+    @EventBusSubscriber(modid = CommunityMod.MODID, bus = Bus.MOD)
+    public static final class ModEvents {
 
-                //Structures
-                register(BeanPieces.BEAN_TYPE, "bean_structure");
-                StructureInit.setupStructures();
-                ConfiguredStructures.registerConfiguredStructures();
+        @SubscribeEvent
+        public static void commonSetup(final FMLCommonSetupEvent event) {
+            DimensionInit.setup();
+
+            //Structures
+            register(BeanPieces.BEAN_TYPE, "bean_structure");
+            StructureInit.setupStructures();
+            ConfiguredStructures.registerConfiguredStructures();
+        }
+
+        @SubscribeEvent
+        public static void gatherData(final GatherDataEvent event) {
+            DataGenerator generator = event.getGenerator();
+            ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+            if (event.includeServer()) {
+                //TODO: recipes, loot tables, tags
             }
-
-            @SubscribeEvent
-            public static void gatherData(final GatherDataEvent event) {
-                DataGenerator generator = event.getGenerator();
-                ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
-
-                if (event.includeServer()) {
-                    //TODO: recipes, loot tables, tags
-                }
-                if (event.includeClient()) {
-                    generator.addProvider(new ModDataGeneration.LanguageGen(generator, "en_us"));
+            if (event.includeClient()) {
+                generator.addProvider(new ModDataGeneration.LanguageGen(generator, "en_us"));
 				/* TODO: change textures path to /item and /block etc. else it won't work!
 				generator.addProvider(
 						new ModDataGeneration.ItemModelGen(generator, existingFileHelper));
 				generator.addProvider(
 						new ModDataGeneration.BlockStateGen(generator, existingFileHelper));
 				 */
-                }
-            }
-
-            @SubscribeEvent
-            public static void entityAttributes(final EntityAttributeCreationEvent event) {
-                event.put(EntityInit.BEAN_ENTITY.get(), BeanEntity.createAttributes().build());
             }
         }
 
-        private static StructurePieceType register(StructurePieceType structurePiece, String key) {
-            return Registry.register(Registry.STRUCTURE_PIECE, key.toLowerCase(Locale.ROOT), structurePiece);
+        @SubscribeEvent
+        public static void entityAttributes(final EntityAttributeCreationEvent event) {
+            event.put(EntityInit.BEAN_ENTITY.get(), BeanEntity.createAttributes().build());
         }
+
     }
+
 }
