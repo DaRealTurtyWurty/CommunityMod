4e9a14444d4169b3ab47820d932e7ba752220e01
Ninjaeevee500 <Ninjaeevee500@users.noreply.github.com>
2021-08-18 14:04:36 +0100

    Amended Pull request

--- src/main/java/io/github/communitymod/core/util/enums/ModToolMaterials.java
+++ src/main/java/io/github/communitymod/core/util/enums/ModToolMaterials.java
@@ -2,23 +2,20 @@ package io.github.communitymod.core.util.enums;
 
 import java.util.function.Supplier;
 
+import com.google.common.base.Suppliers;
 import io.github.communitymod.core.init.ItemInit;
-import net.minecraft.util.LazyLoadedValue;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
 
-@SuppressWarnings("deprecation")
 public enum ModToolMaterials implements Tier {
-	   BEAN(0, 118, 2.0F, 0.0F, 15, () -> {
-	      return Ingredient.of(ItemInit.BEANS.get());
-	   });
+	   BEAN(0, 118, 2.0F, 0.0F, 15, () -> Ingredient.of(ItemInit.BEANS.get()));
 
 	   private final int level;
 	   private final int uses;
 	   private final float speed;
 	   private final float damage;
 	   private final int enchantmentValue;
-	   private final LazyLoadedValue<Ingredient> repairIngredient;
+	   private final Supplier<Ingredient> repairIngredient;
 
 	   private ModToolMaterials(int harvestLevel, int uses, float attackSpeed, float damage, int enchantValue, Supplier<Ingredient> repairIngredient) {
 	      this.level = harvestLevel;
@@ -26,7 +23,7 @@ public enum ModToolMaterials implements Tier {
 	      this.speed = attackSpeed;
 	      this.damage = damage;
 	      this.enchantmentValue = enchantValue;
-	      this.repairIngredient = new LazyLoadedValue<>(repairIngredient);
+	      this.repairIngredient = Suppliers.memoize(repairIngredient::get);
 	   }
 	   
 	   public int getUses() {
