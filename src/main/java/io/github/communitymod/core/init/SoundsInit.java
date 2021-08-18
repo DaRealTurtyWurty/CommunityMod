5fc4d8fe04036dd506f565c73da2c55c9c97c47d
MaroonShaded <maroonshaded@gmail.com>
2021-08-18 20:29:31 +0100

    Optimize imports I guess
    Intellij did that
    Also do a //TODO someone else had put in

--- src/main/java/io/github/communitymod/core/init/SoundsInit.java
+++ src/main/java/io/github/communitymod/core/init/SoundsInit.java
@@ -7,6 +7,9 @@ import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class SoundsInit {
 
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister
