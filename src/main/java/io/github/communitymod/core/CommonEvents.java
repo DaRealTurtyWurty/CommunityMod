1e7637a190a72ed483a268e3db62b39916b6ac65
MaroonShaded <maroonshaded@gmail.com>
2021-08-18 20:37:08 +0100

    Separate CommonEvents inner classes

--- src/main/java/io/github/communitymod/core/CommonEvents.java
+++ src/main/java/io/github/communitymod/core/CommonEvents.java
@@ -33,64 +33,64 @@ import java.util.Map;
 
 public final class CommonEvents {
 
-    @EventBusSubscriber(modid = CommunityMod.MODID, bus = Bus.FORGE)
-    public static final class ForgeEvents {
+	@EventBusSubscriber(modid = CommunityMod.MODID, bus = Bus.FORGE)
+	public static final class ForgeEvents {
 
-        @SubscribeEvent
-        public static void biomeModification(final BiomeLoadingEvent event) {
-            event.getGeneration().getStructures().add(() -> ConfiguredStructures.CONFIGURE_TEST_STRUCTURE);
-        }
+		@SubscribeEvent
+		public static void biomeModification(final BiomeLoadingEvent event) {
+			event.getGeneration().getStructures().add(() -> ConfiguredStructures.CONFIGURE_TEST_STRUCTURE);
+		}
 
-        @SubscribeEvent
-        public static void addDimensionalSpacing(final WorldEvent.Load event) {
-            if (event.getWorld() instanceof ServerLevel serverLevel) {
+		@SubscribeEvent
+		public static void addDimensionalSpacing(final WorldEvent.Load event) {
+			if (event.getWorld() instanceof ServerLevel serverLevel) {
 
-                Map<StructureFeature<?>, StructureFeatureConfiguration> tempMap = new HashMap<>(serverLevel.getChunkSource().generator.getSettings().structureConfig());
-                tempMap.putIfAbsent(StructureInit.BEAN_STRUCTURE.get(), StructureSettings.DEFAULTS.get(StructureInit.BEAN_STRUCTURE.get()));
-                serverLevel.getChunkSource().generator.getSettings().structureConfig = tempMap;
-            }
-        }
+				Map<StructureFeature<?>, StructureFeatureConfiguration> tempMap = new HashMap<>(serverLevel.getChunkSource().generator.getSettings().structureConfig());
+				tempMap.putIfAbsent(StructureInit.BEAN_STRUCTURE.get(), StructureSettings.DEFAULTS.get(StructureInit.BEAN_STRUCTURE.get()));
+				serverLevel.getChunkSource().generator.getSettings().structureConfig = tempMap;
+			}
+		}
+	}
 
-        @EventBusSubscriber(modid = CommunityMod.MODID, bus = Bus.MOD)
-        public static final class ModEvents {
+	@EventBusSubscriber(modid = CommunityMod.MODID, bus = Bus.MOD)
+	public static final class ModEvents {
 
-            @SubscribeEvent
-            public static void commonSetup(final FMLCommonSetupEvent event) {
-                DimensionInit.setup();
+		@SubscribeEvent
+		public static void commonSetup(final FMLCommonSetupEvent event) {
+			DimensionInit.setup();
 
-                //Structures
-                register(BeanPieces.BEAN_TYPE, "bean_structure");
-                StructureInit.setupStructures();
-                ConfiguredStructures.registerConfiguredStructures();
-            }
+			//Structures
+			register(BeanPieces.BEAN_TYPE, "bean_structure");
+			StructureInit.setupStructures();
+			ConfiguredStructures.registerConfiguredStructures();
+		}
 
-            @SubscribeEvent
-            public static void gatherData(final GatherDataEvent event) {
-                DataGenerator generator = event.getGenerator();
-                ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+		@SubscribeEvent
+		public static void gatherData(final GatherDataEvent event) {
+			DataGenerator generator = event.getGenerator();
+			ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
-                if (event.includeServer()) {
-                    //TODO: recipes, loot tables, tags
-                }
-                if (event.includeClient()) {
-                    generator.addProvider(new ModDataGeneration.LanguageGen(generator, "en_us"));
+			if (event.includeServer()) {
+				//TODO: recipes, loot tables, tags
+			}
+			if (event.includeClient()) {
+				generator.addProvider(new ModDataGeneration.LanguageGen(generator, "en_us"));
 				/* TODO: change textures path to /item and /block etc. else it won't work!
 				generator.addProvider(
 						new ModDataGeneration.ItemModelGen(generator, existingFileHelper));
 				generator.addProvider(
 						new ModDataGeneration.BlockStateGen(generator, existingFileHelper));
 				 */
-                }
-            }
+			}
+		}
 
-            @SubscribeEvent
-            public static void entityAttributes(final EntityAttributeCreationEvent event) {
-                event.put(EntityInit.BEAN_ENTITY.get(), BeanEntity.createAttributes().build());
-            }
-        }
+		@SubscribeEvent
+		public static void entityAttributes(final EntityAttributeCreationEvent event) {
+			event.put(EntityInit.BEAN_ENTITY.get(), BeanEntity.createAttributes().build());
+		}
+	}
 
-        private static StructurePieceType register(StructurePieceType structurePiece, String key) {
-            return Registry.register(Registry.STRUCTURE_PIECE, key.toLowerCase(Locale.ROOT), structurePiece);
-        }
-    }
+	private static StructurePieceType register(StructurePieceType structurePiece, String key) {
+		return Registry.register(Registry.STRUCTURE_PIECE, key.toLowerCase(Locale.ROOT), structurePiece);
+	}
 }
