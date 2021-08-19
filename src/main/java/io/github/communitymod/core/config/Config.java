c57c470a5a3e6bf1af97aa8b4ab56d5eeddc9b81
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2021-08-19 18:44:01 +0100

    Fix Everything (#28)
    * Fix Everything
    
    - Follow new style guide
    - Remove data generation
    
    * Update SpecialItem.java

--- src/main/java/io/github/communitymod/core/config/Config.java
+++ src/main/java/io/github/communitymod/core/config/Config.java
@@ -1,16 +1,17 @@
 package io.github.communitymod.core.config;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import io.github.communitymod.core.init.ItemInit;
 import net.minecraftforge.common.ForgeConfigSpec;
-import org.apache.commons.lang3.tuple.Pair;
 
 public class Config {
 
-	public static final ForgeConfigSpec COMMON_SPEC;
-	public static final Common COMMON;
-
-	public static final ForgeConfigSpec CLIENT_SPEC;
 	public static final Client CLIENT;
+	public static final ForgeConfigSpec CLIENT_SPEC;
+
+	public static final Common COMMON;
+	public static final ForgeConfigSpec COMMON_SPEC;
 
 	static {
 		final Pair<Common, ForgeConfigSpec> commonSpecPair = new ForgeConfigSpec.Builder().configure(Common::new);
@@ -22,25 +23,25 @@ public class Config {
 		CLIENT = clientSpecPair.getLeft();
 	}
 
+	public static class Client {
+
+		public final ForgeConfigSpec.ConfigValue<String> tabIcon;
+
+		public Client(ForgeConfigSpec.Builder builder) {
+			this.tabIcon = builder.comment("Resource location of the item to apply to the mod's creative tab icon")
+					.define("Creative Tab Icon", ItemInit.BEANS.getId().toString());
+		}
+	}
+
 	public static class Common {
 
 		public final ForgeConfigSpec.ConfigValue<String> example;
 
 		public Common(ForgeConfigSpec.Builder builder) {
 			builder.comment("Example category. You can remove this if you add a new one.").push("example");
-			example = builder.comment("Example config field. You can remove this if you add a new one.")
+			this.example = builder.comment("Example config field. You can remove this if you add a new one.")
 					.define("Example", "Beans");
 			builder.pop();
-		}
-	}
-
-	public static class Client {
-
-		public final ForgeConfigSpec.ConfigValue<String> tabIcon;
-
-		public Client(ForgeConfigSpec.Builder builder) {
-			tabIcon = builder.comment("Resource location of the item to apply to the mod's creative tab icon")
-					.define("Creative Tab Icon", ItemInit.BEANS.getId().toString());
 		}
 	}
 }
